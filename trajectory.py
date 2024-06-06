087ce4457f4984887e84182a6402430d529b6ec9
elkevdk <elkevanderkooij@gmail.com>
2024-06-06 12:59:01 +0200

    trajectory class

---
+++ trajectory.py
@@ -0,0 +1,35 @@
+import random
+from loading_data import LoadData
+
+class Trajectories:
+    def __init__(self, all_connections, max_time):
+        self.connections = []
+        self.max_time = max_time
+
+    def add_trajectory(self):
+        self.time = 0
+        all_connections_list = list(all_connections.keys())
+        start_station = random.choice(all_connections_list)
+        self.connections.append(start_station)
+
+        current_station = start_station
+        while self.time <= self.max_time:
+            possible_connections = list(all_connections[current_station].keys())
+            next_station = possible_connections[random.randint(0, len(possible_connections) - 1)]
+            travel_time = all_connections[current_station][next_station]
+
+            if self.time + travel_time > self.max_time:
+                break
+
+            self.connections.append(next_station)
+            self.time += travel_time
+            current_station = next_station
+            print(self.time)
+
+        return self.connections
+
+# data = LoadData('files/ConnectiesHolland.csv', 'files/StationsHolland.csv')
+# all_connections = data.connections
+#
+# trajectory = Trajectories(all_connections, 120)
+# print(trajectory.add_trajectory())
