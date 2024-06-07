a988657ca7e69c59a29aba98620ec52c9a3d1bef
elkevdk <elkevanderkooij@gmail.com>
2024-06-07 11:27:24 +0200

    doc strings, comments experiment_class visualize

--- experiment_class.py
+++ experiment_class.py
@@ -2,13 +2,43 @@ from trajectory import Trajectories
 from loading_data import LoadData
 
 class Experiment():
+    """
+    Class that runs an experiment with a set of trajectories.
+
+    Parameters
+    ----------
+    max_trajectories : int
+        Maximum number of trajectories to generate.
+    all_connections : dict
+        Dictionary containing all possible connections between stations and their durations.
+
+    Attributes
+    ----------
+    max_trajectories : int
+        Maximum number of trajectories to generate.
+    all_trajectories : dict
+        Dictionary to store trajectories.
+    all_connections : dict
+        Dictionary containing all possible connections between stations and their durations.
+
+    Methods
+    -------
+    make_pairs()
+        Creates unique pairs of connected stations.
+    run()
+        Generates trajectories and stores them in all_trajectories.
+    """
     def __init__(self, max_trajectories, all_connections):
         self.max_trajectories = max_trajectories
         self.all_trajectories = {}
         self.all_connections = all_connections
 
     def make_pairs(self):
+        # initialize set to store pairs in
         connection_pairs = set()
+        # loop over stations and connections and store each possible connection
+        # as a seperate pair in a set to ensure that the order of the stations
+        # doesn't matter and filter duplicates
         for start_station, connections in self.all_connections.items():
             station_names = list(connections.keys())
 
@@ -16,25 +46,12 @@ class Experiment():
                 # use frozenset inside set
                 connection_pairs.add(frozenset((start_station, station_names[i])))
 
-        print(connection_pairs)
 
     def run(self):
         for i in range(0, self.max_trajectories):
+            # create a trajectory instance with a maximum time fo 120
             trajectory = Trajectories(self.all_connections, 120)
+            # add generated trajectory to dictionary
             self.all_trajectories[f"train_{i + 1}"] = trajectory.add_trajectory()
 
         return self.all_trajectories
-
-    # def generate_output(self):
-    #     output = ""
-    #     for i, route in enumerate(self.routes, 1):
-    #         output += f"train_{i},{route}\n"
-    #
-    #     score = self.calculate_score()
-    #     output += f"score,{score}"
-    #
-    #     return output
-    #
-    # def calculate_score(self):
-    #     p =
-    #     k =
