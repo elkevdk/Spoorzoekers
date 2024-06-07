acf5b557f37a14835f8706e1c888e102e16c7ee7
elkevdk <elkevanderkooij@gmail.com>
2024-06-07 11:00:06 +0200

    comments trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -12,54 +12,75 @@ class Trajectories:
         all_connections : dict
             Contains all possible connections between stations and the corresponding
             duration.
+        max_time : int
+            Maximum time a trajectory is allowed to take.
 
         Attributes
         ----------
-        speed : float
-            speed of the creature.
+        max_time : int
+            Maximum time for the trajectory.
+        all_connections : dict
+            All possible connections between stations and their durations.
+        connections : list
+            List of stations in the current trajectory
+        time : int
+            Elapsed time in current trajectory.
 
         Methods
         -------
-        step()
-            simulate step of creature.
+        add_trajectory()
+            Generates a trajectory within the allowed maximum time.
         """
     def __init__(self, all_connections, max_time):
         self.max_time = max_time
         self.all_connections = all_connections
 
     def add_trajectory(self):
+        # initialize list of connections for the current trajectory
         self.connections = []
+        # initialize elapsed time
         self.time = 0
+        # turn the outer dictionary into a list
         all_connections_list = list(self.all_connections.keys())
+        # select a random starting station
         start_station = random.choice(all_connections_list)
+        # add starting station to trajectory
         self.connections.append(start_station)
 
         current_station = start_station
         previous_station = None
 
-
+        # continue filling trajectory until max time is exceeded
         while self.time <= self.max_time:
+            # retrieve list of possible connections from the current station
             possible_connections = list(self.all_connections[current_station].keys())
 
-            # Try to exclude the previous station from the possible connections
+            # try to exclude the previous station from the possible connections
             if previous_station:
-                # Only exclude if there are other options available
+                # only exclude if there are other stations available
                 if len(possible_connections) > 1:
+                    # use list comprehension to store all stations in possible all_connections
+                    # that are not equal to the previous station
                     possible_connections = [station for station in possible_connections if station != previous_station]
 
-            # If no connections are available, break the loop
+            # if no connections are available, break the loop
             if not possible_connections:
                 break
 
+            # select next station randomly
             next_station = random.choice(possible_connections)
             travel_time = self.all_connections[current_station][next_station]
 
+            # break the loop if adding the next station exceeds the max time
             if self.time + travel_time > self.max_time:
                 break
 
+            # add next station to the current trajectory
             self.connections.append(next_station)
+            # update corresponding time
             self.time += travel_time
-            previous_station = current_station  # Update previous station
-            current_station = next_station  # Move to the next station
+            # set previous station to the current station for the next iteration
+            previous_station = current_station 
+            current_station = next_station
 
         return self.connections
