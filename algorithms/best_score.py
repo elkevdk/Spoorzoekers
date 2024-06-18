8b7e12de3cb9285d7302499314d55c12093ca979
elkevdk <elkevanderkooij@gmail.com>
2024-06-18 15:05:46 +0200

    calculate_p_for_trajecteroy aangepast

--- algorithms/best_score.py
+++ algorithms/best_score.py
@@ -8,12 +8,8 @@ class Score_Optimizer(Random_R):
 
         for connection in possible_connections:
             current_trajectory = self.connections[:]
-            print("cj", current_trajectory)
             current_trajectory.append(connection)
-            print("possible_connections", possible_connections)
-            print("connection", connection)
             score = self.calculate_score_for_trajectory(current_trajectory)
-            print("score", score)
             possible_connections_scores.append(score)
 
         best_connection_index = possible_connections_scores.index(max(possible_connections_scores))
