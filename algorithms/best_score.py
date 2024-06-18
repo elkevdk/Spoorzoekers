f17f350e35b399c936c36840274f058b2273458b
elkevdk <elkevanderkooij@gmail.com>
2024-06-18 13:53:01 +0200

    aanpassingen

--- algorithms/best_score.py
+++ algorithms/best_score.py
@@ -1,15 +1,19 @@
-from algorithms.random_NR import Random_NR
+from algorithms.random_R import Random_R
 from algorithms.base import Base
 
-class Score_Optimizer(Random_NR):
+class Score_Optimizer(Random_R):
 
     def select_next_station(self, possible_connections):
         possible_connections_scores = []
 
         for connection in possible_connections:
             current_trajectory = self.connections[:]
+            print("cj", current_trajectory)
             current_trajectory.append(connection)
-            score = self.calculate_p_for_trajectory(current_trajectory)
+            print("possible_connections", possible_connections)
+            print("connection", connection)
+            score = self.calculate_score_for_trajectory(current_trajectory)
+            print("score", score)
             possible_connections_scores.append(score)
 
         best_connection_index = possible_connections_scores.index(max(possible_connections_scores))
