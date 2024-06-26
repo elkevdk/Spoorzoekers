74a5a81aabfc5055273ef7fe591a11de87a5107c
elkevdk <elkevanderkooij@gmail.com>
2024-06-26 11:52:37 +0200

    command line argumnets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from classes.loading_data import LoadData
 from classes.score_distribution import ScoreDistribution
 from classes.hillclimber_score import HillClimberScore
@@ -12,14 +13,68 @@ from output.visualize2 import PlotTrains
 import os
 
 def main():
+
+    # construct command line arguments
+    parser = argparse.ArgumentParser(description='Run train network optimization algorithms.')
+    parser.add_argument('--algorithm', choices=['random_r', 'random_nr', 'score_optimizer', 'greedy', 'hill_climber', 'score_distribution'], required=True, help='The algorithm to run.')
+    parser.add_argument('--region', choices=['Holland', 'Nederland'], required=True, help='The region to run the algorithm on.')
+    parser.add_argument('--runs', type=int, default=1, help='Number of runs for the hill climber and score distribution algorithms.')
+    parser.add_argument('--remove_counts', type=int, nargs='+', default=[1, 2, 3, 4], help='Number of removals for the hill climber score distribution.')
+    parser.add_argument('--max_trajectories', type=int, default=7, help='Max number of trajectories.')
+    parser.add_argument('--max_time', type=int, default=120, help='Max time for a trajectory.')
+
+    args = parser.parse_args()
+
     # create the output folder if it doesn't exist
     if not os.path.exists('output'):
         os.makedirs('output')
 
-    # ===data for Noord-Holland===
-    data = LoadData('data/ConnectiesHolland.csv', 'data/StationsHolland.csv')
+    # load data based on region
+    if args.region == 'Holland':
+        data = LoadData('data/ConnectiesHolland.csv', 'data/StationsHolland.csv')
+    else:
+        data = LoadData('data/ConnectiesNationaal.csv', 'data/StationsNationaal.csv')
+
     all_connections = data.connections
     all_stations = data.stations
+
+    if args.algorithm == 'random_r':
+        random_r = Random_R(args.max_trajectories, all_connections, args.max_time)
+        random_r.add_trajectory()
+        random_r_results = Base(random_r.all_trajectories, random_r.trajectory_count, all_connections)
+        random_r_results.calculate_score()
+        random_r_results.to_csv(f'output/{args.region}/output_r.csv')
+
+    elif args.algorithm == 'random_nr':
+        random_nr = Random_NR(args.max_trajectories, all_connections, args.max_time)
+        random_nr.add_trajectory()
+        random_nr_results = Base(random_nr.all_trajectories, random_nr.trajectory_count, all_connections)
+        random_nr_results.calculate_score()
+        random_nr_results.to_csv(f'output/{args.region}/output_nr.csv')
+
+    elif args.algorithm == 'score_optimizer':
+        score_optimizer = Score_Optimizer(args.max_trajectories, all_connections, args.max_time)
+        score_optimizer.add_trajectory()
+        score_optimizer_results = Base(score_optimizer.all_trajectories, score_optimizer.trajectory_count, all_connections)
+        score_optimizer_results.calculate_score()
+        score_optimizer_results.to_csv(f'output/{args.region}/output_so.csv')
+
+    elif args.algorithm == 'greedy':
+        greedy = Greedy(args.max_trajectories, all_connections, args.max_time)
+        greedy.add_trajectory()
+        greedy_results = Base(greedy.all_trajectories, greedy.trajectory_count, all_connections)
+        greedy_results.calculate_score()
+        greedy_results.to_csv(f'output/{args.region}/output_greedy.csv')
+
+    elif args.algorithm == 'hill_climber':
+        hill_climber = HillClimber(args.max_trajectories, all_connections, args.max_time)
+        final_trajectories, final_score = hill_climber.run()
+        hill_climber_results = Base(final_trajectories, hill_climber.trajectory_count, all_connections)
+        hill_climber_results.to_csv(f'output/{args.region}/output_hill_climber.csv')
+
+    elif args.algorithm == 'score_distribution':
+        score = HillClimberScore([args.runs], args.remove_counts, all_connections, 100, args.max_trajectories, args.max_time)
+        score.run_distributions()
 
     # # random return algorithm
     # random_r = Random_R(7, all_connections, 120)
@@ -62,10 +117,10 @@ def main():
     # hill_climber_results.to_csv('output/Holland/output_hill_climber.csv')
 
     # Calculate and plot score distribution
-    amount_runs = [500, 1000]
-    remove_counts = [1, 2, 3, 4]
-    score = HillClimberScore(amount_runs, remove_counts, all_connections, 100, 7, 120)
-    score.run_distributions()
+    # amount_runs = [500, 1000]
+    # remove_counts = [1, 2, 3, 4]
+    # score = HillClimberScore(amount_runs, remove_counts, all_connections, 100, 7, 120)
+    # score.run_distributions()
 
     # calculate and plot score distribution
     # ScoreDistribution(20000, Random_R, all_connections, 'output/Holland/score_distribution_r.png', 'Score Distribution Random Return', 7, all_connections, 120)
