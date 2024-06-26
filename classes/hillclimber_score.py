58e47da3cf1167c10536c113d667046931e6a703
Mila Vogels <milavogels@gmail.com>
2024-06-26 16:47:03 +0200

    comments

--- classes/hillclimber_score.py
+++ classes/hillclimber_score.py
@@ -2,6 +2,26 @@ from algorithms.hillclimber import HillClimber
 import matplotlib.pyplot as plt
 
 class HillClimberScore():
+    """
+    Class to run multiple hill-climbing optimizations and generate score distributions.
+
+    Parameters
+    ----------
+    amount_runs : list of int
+        List of different numbers of runs for the hill-climbing algorithm.
+    remove_counts : list of int
+        List of different numbers of trajectories to remove in each iteration.
+    all_connections : dict
+        Dictionary containing all possible connections between stations and their durations.
+    iterations : int
+        Number of iterations to run for each combination of runs and remove_counts.
+    trajectories : int
+        Maximum number of trajectories to generate.
+    time : int
+        Maximum time a trajectory is allowed to take.
+    region : str
+        Region name for the output directory.
+    """
     def __init__(self, amount_runs, remove_counts, all_connections, iterations, trajectories, time, region):
         self.amount_runs = amount_runs
         self.remove_counts = remove_counts
@@ -12,6 +32,13 @@ class HillClimberScore():
         self.region = region
 
     def run_distributions(self):
+        """
+        Run hill-climbing optimizations and generate score distributions.
+
+        For each combination of runs and remove_counts, this method runs the hill-climbing
+        optimization for the specified number of iterations, collects the final scores,
+        and plots a histogram of the score distribution. The histogram is saved to a file in the output folder.
+        """
         scores = []
         for runs in self.amount_runs:
             for count in self.remove_counts:
@@ -23,6 +50,7 @@ class HillClimberScore():
                     if i % 10 == 0 and i != 0:
                         print(f"Iteration {i}")
 
+                # plot and save the histogram
                 plt.figure(figsize=(10, 6))
                 plt.hist(scores, bins=20, edgecolor='black')
                 plt.title(f'Score Distribution Hill Climber, Holland. Runs: {runs} Trajectories Removed: {count}')
